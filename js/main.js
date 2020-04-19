500b7d5b0c95678c909232a00c4b49b57c68c931
Jess Eng <jesseng@jesss-mbp-2.lan>
2020-04-18 23:29:43 -0700

    new project page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
-$("body").on('img', "contextmenu", function(e){ 
-    return false; 
+// function preload(arrayOfImages) {
+//   $(arrayOfImages).each(function(){
+//       $('<img/>')[0].src = this;
+//       // Alternatively you could use:
+//       // (new Image()).src = this;
+//   });
+// }
+
+// // Usage:
+
+// preload([
+//   'p1.jpg',
+//   'p2.jpg',
+//   'p3.jpg', 
+//   'p4.jpg',
+//   'p5.jpg',
+//   'p6.jpg'
+// ]);
+
+$.fn.preload = function() {
+  this.each(function(){
+      $('<img/>')[0].src = this;
   });
+}
+
+// Usage:
+
+$(['p1.jpg','p2.jpg','p3.jpg', 'p4.jpg','p5.jpg','p6.jpg']).preload();
